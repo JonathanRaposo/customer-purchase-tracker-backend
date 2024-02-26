466b6e7b39a40a3cd0b17cbe0c4b017de9f3fd87
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2024-02-26 08:08:06 -0500

    modify

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
+
 
 const isAuthenticated = (req, res, next) => {
     const token = getTokenFromHeaders(req)
